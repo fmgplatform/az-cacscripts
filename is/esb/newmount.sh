5727e5717de910a1a996d74a970490e0b3af0bcb
kevin dessam xavier <kevin.dessamxavier@fmg.co.nz>
2023-11-16 15:34:03 +1300

    fstab value changed

--- is/esb/newmount.sh
+++ is/esb/newmount.sh
@@ -37,7 +37,8 @@ fi
 # Map storage account to drive if required parameters exist.
 if [ ! -z $3 ]; then
   #added in code to check if the map data command is already in the fstab file
-  MAPVALUE="//mount -t nfs $1.file.core.windows.net/$2 $3 -o vers=4,minorversion=1,sec=sys,nconnect=4"
+  MAPVALUE="$1.file.core.windows.net/$2 $3 nfs  vers=4,minorversion=1,sec=sys"
+
   if !( grep //etc/fstab -e "$MAPVALUE" ); then
     sudo echo $MAPVALUE  >> /etc/fstab 
      echo "mappped $MAPVAULE"
